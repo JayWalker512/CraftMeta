ca3fbfcabde69c37efe634f053d9c8c3485888db
Brandon Foltz <bsfoltzguitar@gmail.com>
2017-01-04 20:29:28 -0500

    Initial commit

---
+++ map_tool/js/index.js
@@ -0,0 +1,44 @@
+function coordToRegion(coord) {
+	return Math.floor((coord / 16) / 32.0);
+}
+
+function calculateRegionInfixString(Xcoord, Ycoord) {
+	$('#region').html("You are in region " + coordToRegion(Xcoord) + ',' + coordToRegion(Ycoord) + ". The white circle indicates your exact position.");
+	return coordToRegion(Xcoord) + '.' + coordToRegion(Ycoord);
+}
+
+function validateNumericInput(fieldSelector, errMsg) {
+	if ( $.isNumeric(fieldSelector) == false ) {
+		alert(errMsg);
+		return false;
+	}
+	return true;
+}
+
+function showLocalMap() {
+	var $Xcoord = $('#x-coord');
+	var $Ycoord = $('#y-coord');
+	var errMsg = "Coordinate input must be numeric.";
+	if (false == (validateNumericInput($Xcoord.val(), errMsg) && validateNumericInput($Ycoord.val(), errMsg))) {
+		return; //do nothing
+	}
+
+	var regionInfixString = calculateRegionInfixString(parseInt($Xcoord.val()), parseInt($Ycoord.val()));
+	var $localMapImage = $('#local-map');
+	var downloadingImage = new Image();
+	downloadingImage.onload = function() {
+		$localMapImage.attr("src", this.src);
+
+		var c = document.getElementById("map-canvas");
+    	var ctx = c.getContext("2d");
+    	var img = downloadingImage; //document.getElementById("local-map");
+    	ctx.drawImage(img, 0, 0);
+    	ctx.strokeStyle = "#FFFFFF";
+    	ctx.beginPath();
+    	ctx.arc(parseInt($Xcoord.val()) % 512, parseInt($Ycoord.val()) % 512,20,0,2*Math.PI);
+    	ctx.stroke();
+	};
+
+	downloadingImage.src = "map/tile." + regionInfixString + ".png";
+	console.log(downloadingImage.src);
+}
